0dd892020f2971715b039e138990215dee1ca05c
Florian <f.schaessens@gmail.com>
2024-01-24 21:59:05 +0100

    add animation

--- src/pages/Planets.jsx
+++ src/pages/Planets.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "../components/NavBar"
 import MainInfos from "../components/MainInfos"
 import Data from "../components/Data";
+import Animations from "../components/Animations";
 
 function Planets () {
     const [selectedPlanet, setSelectedPlanet] = useState("Mercury");
@@ -16,8 +17,9 @@ function Planets () {
             <Navbar onSelectPlanet={handleSelectPlanet}/>
             <main>
                 <MainInfos selectedPlanet={selectedPlanet}/>
-                <Data selectedPlanet={selectedPlanet}/>
             </main>
+                <Data selectedPlanet={selectedPlanet}/>
+                <Animations/>
         </>
     )
 }
